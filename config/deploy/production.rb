17ba5eefb876b257d5612b50a9f5c9ebc736db01
矢野 善尊 <yancha.comparr1027@gmail.com>
2019-02-19 01:04:53 +0900

    Implementation of basic authentication

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
 server '54.248.186.131', user: 'ec2-user', roles: %w{app db web}
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
