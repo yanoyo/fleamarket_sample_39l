41a9b90128599200231e4c3a2d9e4c7fe3e15649
yoyano <44337426+yanoyo@users.noreply.github.com>
2019-03-16 01:37:29 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products, only: [:index]
-#   root 'application#hello'
   get  'products/new'  =>  'products#new'
 end
