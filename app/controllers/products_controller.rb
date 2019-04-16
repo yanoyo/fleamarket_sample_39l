c9462a460ffad506638dcbbd51715ff820aa1d56
内田啓太 <keita.uchida.r@gmail.com>
2019-04-16 20:59:32 +0900

    Update-products_controller
    インデントの修正。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,20 +3,6 @@ class ProductsController < ApplicationController
   end
 
   def new
-<<<<<<< HEAD
-  @product = Product.new
-  @product.images.build
-  @categoryroot = Category.find(16).siblings
-  end
-  	
-  def create
-  @product = Product.new(product_params)
-  if @product.save
-  	redirect_to root_path(@product)
-  else
-	redirect_to new_product_path
-  end
-=======
     @product = Product.new
     @product.images.build
     @categoryroot = Category.find(16).siblings
@@ -25,11 +11,10 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to root_path(@product)
+  	  redirect_to root_path(@product)
     else
-      redirect_to new_product_path
+	  redirect_to new_product_path
     end
->>>>>>> origin/Add-product-new-function
   end
 
   def product_list
