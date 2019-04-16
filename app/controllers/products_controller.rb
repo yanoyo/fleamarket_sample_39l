f9cb177e004cf24c84d38f76e3af7954d46a60ff
内田啓太 <keita.uchida.r@gmail.com>
2019-04-16 20:40:32 +0900

    Update-products_controller
    products_controller.rbのインデント修正。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,31 @@
 class ProductsController < ApplicationController
+  def index
+  end
 
-	def index
-	end
+  def new
+  	@product = Product.new
+  	@product.images.build
+  	@categoryroot = Category.find(16).siblings
+  end
+  	
+  def create
+  	@product = Product.new(product_params)
+	if @product.save
+	  redirect_to root_path(@product)
+	else
+      redirect_to new_product_path
+    end
+  end
 
-	def new
-	  @product = Product.new
-	  @product.images.build
-	  @categoryroot = Category.find(16).siblings
-	end
+  def product_list
+  end
 
-	def create
-	  @product = Product.new(product_params)
-	  if @product.save
-	  	redirect_to root_path(@product)
-	  else
-        redirect_to new_product_path
-      end
-	end
+  def buy_confirm
+  end
 
-	def product_list
-	end
-	
-	def buy_confirm
-	end
+  private
+  def product_params
+  	params.require(:product).permit(:name, :description, :category_id, :price, :condition, :shipping_fee, :shipping_method, :shipping_from, :shipping_term, images_attributes: {image: []})
+  end
 
-	private
-	def product_params
-	  params.require(:product).permit(:name, :description, :category_id, :price, :condition, :shipping_fee, :shipping_method, :shipping_from, :shipping_term, images_attributes: {image: []})
-	end
 end
