f4cb4c41f7588a90795faa2babc48116d8d6c881
blackwatcher <blackwatcher.news@gmail.com>
2019-04-16 20:52:36 +0900

    Update products_controller
    インデントの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,20 +3,18 @@ class ProductsController < ApplicationController
   end
 
   def new
-  	@product = Product.new
-  	@product.images.build
-  	@categoryroot = Category.find(16).siblings
+    @product = Product.new
+    @product.images.build
+    @categoryroot = Category.find(16).siblings
   end
   	
   def create
-  	@product = Product.new(product_params)
-	
-	if @product.save
-	  redirect_to root_path(@product)
-	else
-	  redirect_to new_product_path
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path(@product)
+    else
+      redirect_to new_product_path
     end
-  
   end
 
   def product_list
@@ -27,7 +25,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-  	params.require(:product).permit(:name, :description, :category_id, :price, :condition, :shipping_fee, :shipping_method, :shipping_from, :shipping_term, images_attributes: {image: []})
+    params.require(:product).permit(:name, :description, :category_id, :price, :condition, :shipping_fee, :shipping_method, :shipping_from, :shipping_term, images_attributes: {image: []})
   end
 
 end
